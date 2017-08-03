ec59b442dcd8a8c4550ff95af60b2b8a7cfdf04d
Bryan Brown <bbrown98@student.gsu.edu>
2017-08-03 17:20:12 -0400

    Update and rename main.java to colors.java

---
+++ Java/Randomized Color Game/Game/colors.java
@@ -0,0 +1,19 @@
+package Game;
+
+import javax.swing.*;
+
+import Levels.Lvl_1;
+
+public class colors extends JPanel{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	public static int answer;
+	public static boolean active;
+	public static JFrame window1, window2, window3;
+	
+	public static void main(String[] args) {
+		Lvl_1.main(null);
+	}
+}
